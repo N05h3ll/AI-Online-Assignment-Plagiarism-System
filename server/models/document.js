6a93304427912fe73c611a5e1af1708da0384cc3
N05h3ll <m.ashm985@gmail.com>
2021-02-06 02:24:34 +0200

    Removed required fields.

--- server/models/document.js
+++ server/models/document.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 const schema = mongoose.Schema;
 const documentSchema = new schema({
-    name: {type: String, required: true},
-    authorID: {type: mongoose.Schema.Types.ObjectId, required: true},
+    name: {type: String},
+    authorID: {type: mongoose.Schema.Types.ObjectId},
     assignmentID: {type: mongoose.Schema.Types.ObjectId},
-    URL: {type: String, required: true},
-    charCount: {type: mongoose.Schema.Types.Decimal128, required:true},
-    size: {type: mongoose.Schema.Types.Decimal128, required:true},
-    docType: {type: Number, required: true}
+    URL: {type: String},
+    charCount: {type: mongoose.Schema.Types.Decimal128},
+    size: {type: mongoose.Schema.Types.Decimal128},
+    docType: {type: String}
 });
 
 const document = mongoose.model('document', documentSchema);
