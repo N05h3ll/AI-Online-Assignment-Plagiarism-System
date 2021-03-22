c4e3358fa6476ca561cc0a5c87570fee0cbd7f48
N05h3ll <m.ashm985@gmail.com>
2021-03-23 01:54:49 +0200

    Added courseID field, and referenced Object ids.

--- server/models/assignment.js
+++ server/models/assignment.js
@@ -5,15 +5,16 @@ const assignmentSchema = new schema({
     dueDate: {type: Date},
     code: {type: String},
     description: {type: String},
-    authorID: { type: mongoose.Schema.Types.ObjectId },
+    authorID: { type: mongoose.Schema.Types.ObjectId, ref:'user' },
     authorInstitution: { type: String },
     authorName: { type: String },
+    courseID: { type: mongoose.Schema.Types.ObjectId, ref: 'course' },
     submittedStudents: [{
-        studentdID: { type: mongoose.Schema.Types.ObjectId },
+        studentdID: { type: mongoose.Schema.Types.ObjectId, ref:'user' },
         studentdName: { type: String },
         submissionDate: { type: String},
         status: { type: String },
-        reportID: { type: mongoose.Schema.Types.ObjectId }
+        reportID: { type: mongoose.Schema.Types.ObjectId, ref:'report' }
     }]
 });
 
