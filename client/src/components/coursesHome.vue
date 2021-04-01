01a361b0cf7cd5dda34f45e47b971102aa8bea68
N05h3ll <m.ashm985@gmail.com>
2021-04-01 03:15:57 +0200

    Changed module coordinator component to courses home.

---
+++ client/src/components/coursesHome.vue
@@ -0,0 +1,37 @@
+<template>
+<coursesSearch/>
+    <div class="container-fluid row" id="middleContainer">
+
+          <div class="row align-items-center">
+            <div class="col">
+              <div class="d-grid gap-3 col-5 ">
+                <button
+                  class="btn btn-lg btn-warning"
+                  type="button"
+                >
+                  Courses
+                </button>
+              </div>
+            </div>
+<coursesContainer/>
+          </div>
+    </div>
+</template>
+
+<script>
+import coursesContainer from './coursesContainer.vue';
+import coursesSearch from './coursesSearch.vue';
+
+export default {
+  components: {
+    coursesContainer,
+    coursesSearch,
+  },
+};
+</script>
+
+<style scoped>
+.table-responsive {
+  max-height: 50vh;
+}
+</style>
